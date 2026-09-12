95717086c78e4cf85f4f48b17d27592e919f796b
Yonazahran Yoga Meinendra Rizky <yonazhrn@gmail.com>
2026-09-12 12:26:17 +0700

    Update trigger.sql

--- projects/log-parser-ioc-pipeline/trigger.sql
+++ projects/log-parser-ioc-pipeline/trigger.sql
@@ -1,10 +1,10 @@
-DELIMITER $$
-
+DELIMITER //
 CREATE TRIGGER trg_brute_force
 AFTER INSERT ON events
 FOR EACH ROW
 BEGIN
     DECLARE fail_count INT;
+    DECLARE existing_alert INT;
 
     IF NEW.event_id = 4625 THEN
         SELECT COUNT(*) INTO fail_count
@@ -14,12 +14,17 @@ BEGIN
           AND event_time >= (NEW.event_time - INTERVAL 10 MINUTE)
           AND event_time <= NEW.event_time;
 
-        IF fail_count >= 5 THEN
-            INSERT INTO alerts (alert_type, related_account, related_ip, details)
-            VALUES ('Brute Force Suspected', NEW.account, NEW.source_ip,
+        SELECT COUNT(*) INTO existing_alert
+        FROM alerts
+        WHERE related_ip = NEW.source_ip
+          AND alert_type = 'Brute Force Suspected'
+          AND triggered_at >= (NEW.event_time - INTERVAL 10 MINUTE);
+
+        IF fail_count >= 5 AND existing_alert = 0 THEN
+            INSERT INTO alerts (triggered_at, alert_type, related_account, related_ip, details)
+            VALUES (NEW.event_time, 'Brute Force Suspected', NEW.account, NEW.source_ip,
                     CONCAT(fail_count, ' failed logons in 10 min window'));
         END IF;
     END IF;
-END$$
-
+END//
 DELIMITER ;
